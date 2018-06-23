92c94103e4ae4ff3409596567026cadc95e743dc
Marcin Ochyra <mochyra@wsiz.rzeszow.pl>
2018-06-23 10:24:20 +0200

    Wyswietlanie szczegolow

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -3,10 +3,12 @@ package com.example.stud.musicapp.topsongs;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.Track;
 import com.example.stud.musicapp.api.Tracks;
 
 import retrofit2.Call;
@@ -40,10 +42,11 @@ public class SongDetailsActivity extends AppCompatActivity {
         ApiService.getService().getTrack(trackId).enqueue(new Callback<Tracks>() {
             @Override
             public void onResponse(@NonNull Call<Tracks> call, @NonNull Response<Tracks> response) {
-                Toast.makeText(
-                        SongDetailsActivity.this,
-                        "Pobrano dane", Toast.LENGTH_SHORT
-                ).show();
+                Tracks tracks = response.body();
+
+                if (tracks != null && tracks.track.size() > 0) {
+                    showData(tracks.track.get(0));
+                }
             }
 
             @Override
@@ -53,9 +56,20 @@ public class SongDetailsActivity extends AppCompatActivity {
                         "Błąd pobierania danych: " + t.getLocalizedMessage(),
                         Toast.LENGTH_SHORT
                 ).show();
-
             }
         });
+    }
+
+    private void showData(Track track) {
+        TextView tvAlbum = findViewById(R.id.tvAlbum);
+        TextView tvGenre = findViewById(R.id.tvGenre);
+        TextView tvStyle = findViewById(R.id.tvStyle);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+
+        tvAlbum.setText(track.strAlbum);
+        tvGenre.setText(track.strGenre);
+        tvStyle.setText(track.strStyle);
+        tvDescription.setText(track.strDescriptionEN);
     }
 
     @Override
